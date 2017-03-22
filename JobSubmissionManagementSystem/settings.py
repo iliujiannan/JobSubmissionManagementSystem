5b8c78c0d2a959e022c7ba5b79455d8425b83999
yourname <liujiannan@imudges.com>
2017-03-22 15:26:22 +0800

    model

--- JobSubmissionManagementSystem/settings.py
+++ JobSubmissionManagementSystem/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import pymysql
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'JobSubmissionManagementSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'jsms',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
--- web/models.py
+++ web/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    student_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+    num = models.CharField(max_length=255)
+    sex = models.CharField(max_length=255)
+    class_name = models.CharField(max_length=255)
+    profession = models.CharField(max_length=255)
+
+
+class Job(models.Model):
+    job_id = models.IntegerField(primary_key=True)
+    job_title = models.CharField(max_length=255)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+
+class JobSubmission(models.Model):
+    job_submission_id = models.IntegerField(primary_key=True)
+    student_id = models.IntegerField(max_length=11)
+    sub_time = models.DateTimeField()
+
+
+
+
+
